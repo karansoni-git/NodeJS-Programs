a141dbf50b523a00b7674b0abb23e4c925f8fd18
karan soni <karanparekh1011@gmail.com>
2025-07-19 22:11:02 +0530

    Add files via upload

---
+++ 14_MVC/models/homeModel.js
@@ -0,0 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+
+exports.newHome = class newHome {
+  constructor(homeName, homePrice, homeLocation, homeRating, homeUrl) {
+    this.homeName = homeName;
+    this.homePrice = homePrice;
+    this.homeLocation = homeLocation;
+    this.homeRating = homeRating;
+    this.homeUrl = homeUrl;
+  }
+
+  addHome() {
+    newHome.showAllHomes((houseList) => {
+      houseList.push(this);
+      let homeDataPath = path.join(__dirname, "../", "data", "homeData.json");
+      fs.writeFile(homeDataPath, JSON.stringify(houseList), (error) => {
+        if (!error) {
+          console.log("File Written successfully");
+        }
+      });
+    });
+  }
+
+  static showAllHomes(callback) {
+    let homeDataPath = path.join(__dirname, "../", "data", "homeData.json");
+    fs.readFile(homeDataPath, (error, data) => {
+      if (data === null) {
+        console.log("there are no data availabel");
+      } else {
+        callback(!error ? JSON.parse(data) : []);
+      }
+    });
+  }
+};
